c4cf3bdbc67fc43968cdacff10c87e2dd4efaf61
kashyap-shubh <164663073+kashyap-shubh@users.noreply.github.com>
2024-04-10 02:37:01 +0530

    Update variable.tf

--- Day-2/practice/variable_with_default/variable.tf
+++ Day-2/practice/variable_with_default/variable.tf
@@ -1,11 +1,14 @@
 variable "ami_value" {
     description = "value for the ami"
+    default     = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
 }
 
 variable "instance_type_value" {
     description = "value for instance_type"
+    default     = "t2.micro"
 }
 
 variable "subnet_id_value" {
     description = "value for the subnet_id"
+    default     = "10.0.0.0/16"
 }
